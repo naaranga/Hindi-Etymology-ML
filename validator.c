5534ae1b8489cbb27d0f00cf87bacbc9a38df3c1
Naaranga <thealmightycheez@gmail.com>
2026-09-21 16:24:55 -0400

    Completed validator.c and validated data successfully

--- validator.c
+++ validator.c
@@ -6,7 +6,7 @@
 #include <unistd.h>
 #include <stddef.h>
 
-#define FSIZE 128400
+#define FSIZE 128402
 
 int main() {
     // shamelessly reusing the mmap sequence from culler.c
@@ -17,39 +17,53 @@ int main() {
     printf("%zu\n", fs.st_size);*/
     unsigned char *startPtr = mmap(NULL, FSIZE, PROT_READ, MAP_PRIVATE, srcDesc, 0);
     if (startPtr == MAP_FAILED) { close(srcDesc); return 1; }
-    unsigned char *nearA = startPtr,        *farA = startPtr + 13,
-                  *nearB = startPtr + 16,   *farB = nearB,  *endPtr = startPtr + FSIZE;
-    
-    int line = 0;
+    unsigned char *nearL = startPtr,        *farL = startPtr + 15,
+                  *nearU = startPtr + 18,   *farU = nearU,  *endPtr = startPtr + FSIZE;
+
+    int line = 2, diffL = farL - nearL, diffU;
     while (1) {
         while (1) {
-            if (farB == 0xE0) {
-                if ((*(++farB) & 0xFE) != 0xA4) {
+            unsigned char cmp = *farU;
+            if (cmp == 0xE0) {
+                if ((*(++farU) & 0xFE) != 0xA4) {
                     printf("Line %d: Malformed Devanagari (second character)\n", line);
                     return 1;
-                } else if ((*(++farB) & 0xC0) != 0x80) {
+                } else if ((*(++farU) & 0xC0) != 0x80) {
                     printf("Line %d: Malformed Devanagari (third character)\n", line);
                     return 1;
                 } else
-                    farB++;
-            } else if (farB <= 0x7F) {
-                if (farB == 0x0D) {
-                    farB--;
-                    if (farB - nearB <= 0) {
+                    farU++;
+            } else if (cmp <= 0x7F) {
+                if (cmp == 0x0D) {
+                    farU--;
+                    if ((diffU = farU - nearU) <= 0) {
                         printf("Line %d: Empty/malformed line\n", line);
                         return 1;
+                    } else if (diffL == diffU && !strncmp(nearL, nearU, diffL)) {
+                        printf("Line %d: Duplicate line\n", line);
+                        return 1;
                     }
-                    // handle EOL
+                    nearL = nearU;
+                    farL = farU;
+                    diffL = diffU;
+                    if (*(farU += 2) != 0x0A) {
+                        printf("Line %d: Malformed CRLF (second character)\n", line);
+                        return 1;
+                    } else if (++farU == endPtr) {
+                        printf("Processing successful\n");
+                        munmap(startPtr, FSIZE);
+                        close(srcDesc);
+                        return 0;
+                    }
+                    nearU = farU;
                     line++;
+                    break;
                 } else
-                    farB++;
+                    farU++;
             } else {
                 printf("Line %d: Malformed character\n", line);
                 return 1;
             }
         }
     }
-    munmap(startPtr, FSIZE);
-    close(srcDesc);
-    return 0;
 }
